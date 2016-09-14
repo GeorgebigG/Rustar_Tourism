b8e5d37ec35b92f35c398a963326191b1e679e6e
Giorgi Nebieridze <giorgi.nebieridze.gn@gmail.com>
2016-09-14 09:45:35 +0400

    Commit

--- app/src/main/java/com/rustaronline/mobile/rustartourism/Helper/ViewPagerAdapter.java
+++ app/src/main/java/com/rustaronline/mobile/rustartourism/Helper/ViewPagerAdapter.java
@@ -16,13 +16,18 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
         super(fm);
     }
 
+    private static final int HOME_PAGE = 0;
+    private static final int SEARCH_PAGE = 1;
+
+    private static final int AMOUNT_OF_PAGES = 2;
+
     @Override
     public Fragment getItem(int position) {
-        if (position == 0) {
+        if (position == HOME_PAGE) {
             FirstPage firstPage = new FirstPage();
             return firstPage;
         }
-        else if (position == 1) {
+        else if (position == SEARCH_PAGE) {
             Search search = new Search();
             return search;
         } else
@@ -31,6 +36,6 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public int getCount() {
-        return 2;
+        return AMOUNT_OF_PAGES;
     }
 }
